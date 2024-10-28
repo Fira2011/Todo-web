09a8186e81e9e2c1777a589b183cee64993b1ed8
Firafir Mesele <firafirmessele@gmail.com>
2024-10-28 16:39:05 +0300

    Build chakra theme

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  config: {
+    initialColorMode: 'light',
+    useSystemColorMode: false,
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.50',
+        _dark: {
+          bg: 'gray.900',
+        },
+      },
+    },
+  },
+});
